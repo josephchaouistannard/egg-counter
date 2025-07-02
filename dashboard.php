a185a0975653eaeb009f5744f45e0250c9c743db
josephchaouistannard <josephchaouistannard@gmail.com>
2025-07-02 11:15:10 +0200

    webhook test log out

--- dashboard.php
+++ dashboard.php
@@ -41,7 +41,7 @@ function refreshEggRecords()
 <body>
     <header>
         <nav>
-            <a href="logout.php">Logout</a>
+            <a href="logout.php">Log out</a>
         </nav>
     </header>
 
